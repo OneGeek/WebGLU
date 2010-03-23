9c6cb2c097def1f0a341311c07f28ee3e612dae3
Benjamin DeLillo <bpd9116@gmail.com>
2010-03-23 14:59:17 -0400

    Moved loadSylvester to Util.js

--- src/Util.js
+++ src/Util.js
@@ -288,3 +288,109 @@ $W.util.getGLContext= function getGLContext(canvas) {
 
     return gl;
 };
+
+//--------------------------------------------------------------------------
+//
+// augment Sylvester some
+// (c) 2009 Vladimir Vukicevic
+$W.util.loadSylvester = function() {
+    $W.util.include($W.paths.external + $W.paths.sylvester);
+
+    Matrix.Translation = function (v) {
+        if (v.elements.length == 2) {
+            var r = Matrix.I(3);
+            r.elements[2][0] = v.elements[0];
+            r.elements[2][1] = v.elements[1];
+            return r;
+        }
+
+        if (v.elements.length == 3) {
+            var r = Matrix.I(4);
+            r.elements[0][3] = v.elements[0];
+            r.elements[1][3] = v.elements[1];
+            r.elements[2][3] = v.elements[2];
+            return r;
+        }
+
+        throw "Invalid length for Translation";
+    }
+
+
+    Matrix.prototype.trace = function() {
+        return this[0][0] + this[1][1] + this[2][2];
+    }
+
+    Matrix.prototype.flatten = function () {
+        var result = [];
+        if (this.elements.length === 0) {
+            return [];
+        }
+
+
+        for (var j = 0; j < this.elements[0].length; j++) {
+            for (var i = 0; i < this.elements.length; i++) {
+                result.push(this.elements[i][j]);
+            }
+        }
+        return result;
+    }
+
+    Matrix.prototype.ensure4x4 = function()
+    {
+        if (this.elements.length == 4 && 
+                this.elements[0].length == 4) {
+            return this;
+        }
+
+        if (this.elements.length > 4 ||
+                this.elements[0].length > 4) {
+            return null;
+        }
+
+        for (var i = 0; i < this.elements.length; i++) {
+            for (var j = this.elements[i].length; j < 4; j++) {
+                if (i == j) {
+                    this.elements[i].push(1);
+                }else {
+                    this.elements[i].push(0);
+                }
+            }
+        }
+
+        for (var i = this.elements.length; i < 4; i++) {
+            if (i === 0) {
+                this.elements.push([1, 0, 0, 0]);
+            }else if (i == 1) {
+                this.elements.push([0, 1, 0, 0]);
+            }else if (i == 2) {
+                this.elements.push([0, 0, 1, 0]);
+            }else if (i == 3) {
+                this.elements.push([0, 0, 0, 1]);
+            }
+        }
+
+        return this;
+    };
+
+    Matrix.prototype.make3x3 = function()
+    {
+        if (this.elements.length != 4 ||
+                this.elements[0].length != 4) {
+            return null;
+        }
+
+        return Matrix.create([[this.elements[0][0], this.elements[0][1], this.elements[0][2]],
+                [this.elements[1][0], this.elements[1][1], this.elements[1][2]],
+                [this.elements[2][0], this.elements[2][1], this.elements[2][2]]]);
+    };
+
+    Vector.prototype.flatten = function () {
+        return this.elements;
+    }; 
+
+    Vector.prototype.vec3Zero = Vector.Zero(3);
+
+    Vector.prototype.invert = function() {
+        return Vector.prototype.vec3Zero.subtract(this);
+    }
+};
