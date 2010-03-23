1c34da3d1460ff86d44d8a6a9739af5052d7ae2f
Benjamin DeLillo <bpd9116@gmail.com>
2010-03-23 13:57:51 -0400

    Force no caching of files loaded through loadFileAsText, but only when the file is located at a localhost, 127.0.0.1, or file:// url

--- src/webglu.js
+++ src/webglu.js
@@ -720,10 +720,14 @@ $W = {
             console.log("Loading file `" + path + "`");
             var xhr = null;
             xhr = new XMLHttpRequest();
+
+            if (!xhr) { 
+                return null; 
+            }
+
             xhr.overrideMimeType("text/xml");
 
-            if (!xhr) { return null; }
-
+            // Deal with firefox security for file:// urls
             try {
                 var nsPM = null;
                 if (typeof(netscape) !== 'undefined' && 
@@ -737,14 +741,26 @@ $W = {
                     }
                 }
             }catch (e) {
-                console.groupEnd();
                 console.error(e);
-                throw "Browser security may be restrcting access to local files";
+                throw "\tBrowser security may be restrcting access to local files";
             }
 
             try {
-                xhr.open("GET", path, false);
-                xhr.send(null);
+
+            xhr.open("GET", path, false);
+            // Ignore cache if the file is served from localhost or is at a
+            // file:// url as it's safe to assume that's a developer.
+            var url = window.location.href;
+            if (    url.match(/^http:\/\/localhost/) !== null ||
+                    url.match(/^http:\/\/127\.0\.0\.1/) !== null ||
+                    url.match(/^file:/) !== null) {
+
+                xhr.setRequestHeader('Pragma', 'Cache-Control: no-cache');
+                //xhr.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 2005 00:00:00 GMT");
+            }
+
+
+            xhr.send(null);
 
             }catch (e) { 
                 throw e; 
