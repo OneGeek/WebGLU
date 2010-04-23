a938c05c463da8b7e89590cf9812cdabc5f6a8e2
Benjamin DeLillo <bpd9116@gmail.com>
2010-04-22 20:31:00 -0400

    Moved textures out and flattened the namespace. Kept old namespace aliased for now to avoid breaking things.

--- src/webglu.js
+++ src/webglu.js
@@ -154,7 +154,7 @@ $W = {
 
 
     /** Draw all objects from the camera's perspective. */
-    draw: function() {
+    draw: function $W_draw() {
         $W.clear();
 
         $W._setupMatrices();
@@ -169,7 +169,7 @@ $W = {
 	},
 
     /** Update all objects. */
-	update : function() {
+	update : function $W_update() {
 		$W._updateState();
 
         for (var i = 0; i < this.objects.length; i++) {
@@ -179,7 +179,7 @@ $W = {
         $W.camera.update();
 	},
 
-    _drawObjects : function() {
+    _drawObjects : function $W_drawObjects() {
         for (var i = 0; i < $W.renderables.length; i++) {
             $W.objects[i].draw();
         }
@@ -450,111 +450,6 @@ $W = {
 
     },
 
-
-    /** @namespace Texture classes */
-    texture:{
-        Texture: function(name) {
-            this.glTexture = $W.GL.createTexture();
-            this.name = name;
-            $W.textures[name] = this;
-
-            this.bind = function() {
-                $W.GL.bindTexture($W.GL.TEXTURE_2D, this.glTexture);
-            };
-                
-            this.unbind = function() {
-                $W.GL.bindTexture($W.GL.TEXTURE_2D, this.glTexture);
-            };
-        },
-
-        Canvas: function(name, src) {
-            $W.texture.Texture.call(this, name);
-
-            this.canvas = src;
-            this.canvas.texture = this;
-
-            this.update = function() {
-                var gl = $W.GL;
-                this.texture.bind();
-                gl.texImage2D(gl.TEXTURE_2D, 0, this);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-                this.texture.unbind();
-            };
-
-        },
-
-        /** A dynamic texture from a `video` element.    
-         * @param {String} name The global name this texture will be referenced
-         * by elsewhere.
-         * @param {String|Video} src Video path or DOM video element.
-         */
-        Video: function(name, src) {
-            $W.texture.Texture.call(this, name);
-
-            this.setSource = function(video) {
-                // Path to video
-                if (typeof(video) === 'string') {
-                    this.video = document.createElement('video');
-                    document.getElementsByTagName('body')[0].appendChild(this.video);
-
-                    this.video.src = video;
-
-                // DOM Video element
-                }else {
-                    this.video = video;
-                }
-
-                this.video.texture = this;
-                this.video.autobuffer = true;
-                this.video.play();
-                this.video.addEventListener("timeupdate", this.update, true);
-            };
-
-            this.update = function() {
-                var gl = $W.GL;
-                this.texture.bind();
-                gl.texImage2D(gl.TEXTURE_2D, 0, this.texture.video);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-                //gl.generateMipmap(gl.TEXTURE_2D);
-                //gl.bindTexture(gl.TEXTURE_2D, null); // clean up after ourselves
-            }
-
-            this.setSource(src);
-        },
-
-        /** A static texture from an image file.
-         * @param {String} name The global name this texture will be referenced
-         * by elsewhere.
-         * @param {String} src Path to image file.
-         */
-        Image: function(name, src) {
-            $W.texture.Texture.call(this, name);
-            this.image = document.createElement('img');
-            this.image.texture = this;
-
-            this.image.onload = function() {
-                var gl = $W.GL;
-                console.group('Loading texture `' + name + "`");
-                this.texture.bind();
-                gl.texImage2D(gl.TEXTURE_2D, 0, this.texture.image);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-                gl.bindTexture(gl.TEXTURE_2D, null); // clean up after ourselves
-                console.log('Done');
-                console.groupEnd();
-            }
-
-            this.setSource = function(src) {
-                this.image.src = src;
-            }
-
-            if (src !== undefined) {
-                this.setSource(src);
-            }
-        }
-    },
 
     // Classes
     /** @class Quaternion implementation.
@@ -878,7 +773,11 @@ $W = {
         },
 
         include: function(path) {
-            console.groupCollapsed('Including ' + path);
+            try{
+                console.groupCollapsed('Including ' + path);
+            }catch(e){
+                console.group('Including ' + path);
+            }
             var script = $W.util.loadFileAsText(path);
             window.eval(script);
             console.groupEnd();
@@ -906,6 +805,13 @@ $W = {
         $W.util.include($W.paths.libsrc + 'GLSL.js');
         $W.util.include($W.paths.libsrc + 'GLU.js');
         $W.util.include($W.paths.libsrc + 'Object.js');
+        $W.util.include($W.paths.libsrc + 'Texture.js');
+
+        $W.texture = {};
+        $W.texture.Texture = $W.Texture;
+        $W.texture.Canvas= $W.CanvasTexture;
+        $W.texture.Video = $W.VideoTexture;
+        $W.texture.Image = $W.ImageTexture;
 
         // Prep the shader subsystem
         $W.GLSL.initialize();
