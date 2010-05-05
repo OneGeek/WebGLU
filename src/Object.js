aef2e70763049f76bd481bfa679f105dc7135da8
Benjamin DeLillo <bpd9116@gmail.com>
2010-05-05 13:00:30 -0400

    Added ArrayBuffer class. Added Object.arrayBuffers. Added Object.material. Removed Object.shaderProgram. Removed Object.textures. Removed Object.buffers. Removed Object.arrays. Removed Object._debugArrays. Removed Object.bind(). Changed Object._children to Object.children. Rewrote fillArray to use arrayBuffers Rewrote Object.drawAt to use the new Renderer.render.

--- src/Object.js
+++ src/Object.js
@@ -1,3 +1,48 @@
+$W.ArrayBuffer = function(name, data) {
+    this.name = name;
+    this.data = data;
+    this.glData = new WebGLFloatArray(data);
+    this.glBuffer = $W.GL.createBuffer();
+
+    this.setData = function ABUF_setData(data) {
+        this.data = data;
+        this.glData = new WebGLFloatArray(data);
+    };
+
+    this.buffer = function ABUF_buffer() {
+        try {
+            this.bind();
+            $W.GL.bufferData($W.GL.ARRAY_BUFFER, this.glData, $W.GL.STATIC_DRAW);
+            this.unbind();
+        }catch (e) {
+            console.error(e);
+        }
+    };
+
+    this.associate = function ABUF_associate(attrib) {
+        try {
+            this.bind();
+            $W.GL.enableVertexAttribArray(attrib.location);
+            //$W.GL.bufferData($W.GL.ARRAY_BUFFER, this.glData, $W.GL.STATIC_DRAW);
+            $W.GL.vertexAttribPointer(attrib.location, attrib.length,
+                    $W.GL.FLOAT, false, 0, 0);
+            this.unbind();
+        }catch (e) {
+            console.error("Failed to associate array buffer `" + this.name + 
+                    "` with vertex attribute `" + attrib.name + "`");
+            console.dir(this, attrib);
+            console.error(e);
+        }
+    };
+
+    this.bind = function ABUF_bind() {
+        $W.GL.bindBuffer($W.GL.ARRAY_BUFFER, this.glBuffer);
+    };
+    this.unbind = function ABUF_unbind() {
+        $W.GL.bindBuffer($W.GL.ARRAY_BUFFER, null);
+    };
+};
+
 
 $W.RENDERABLE   = 1;
 $W.PICKABLE     = 2;
@@ -23,7 +68,7 @@ $W.Object = function (type, flags) {
     //console.group("Creating object");
     $W.ObjectState.call(this);
 
-    $W.addObject(this);
+    $W.objects.push(this);
 
     if (typeof(flags) === 'undefined' ||/*backcompat*/ flags === true) {
         flags = $W.RENDERABLE | $W.PICKABLE;
@@ -40,9 +85,6 @@ $W.Object = function (type, flags) {
         $W.pickables.push(this);
     }
 
-    /** Name of shader program used to render this object */
-    this.shaderProgram = 'default';
-    var lastShaderProgram = 'default';
 
     /** Number of vertices in this object.
      * Used when rendering with drawArrays.
@@ -51,44 +93,184 @@ $W.Object = function (type, flags) {
 
     this.id = $W.createdObjectCount++;
 
-    /* The type of rendering to use for this object, possible values are:<br>
-     * $W.GL.POINTS         <br>
-     * $W.GL.LINES          <br>
-     * $W.GL.LINE_LOOP      <br>
-     * $W.GL.LINE_STRIP     <br>
-     * $W.GL.TRIANGLES      <br>
-     * $W.GL.TRIANGLE_STRIP <br>
-     * $W.GL.TRIANGLE_FAN   <br>
-     */
-    this.type = type; // render type (wglu.GL.LINES, wglu.GL.POINTS, wglu.GL.TRIANGLES, etc.)
+    /* The type of rendering to use for this object */
+    this.type = type; 
 
     this._elements = false;
     this._elementBuffer = null;
     this._elementCount = 0;
 
-    /** WebGL array buffers for attribute data. */
-    this.buffers = [];
+    this.material = $W.materials['wglu_default'];
 
-    /** WebGL*array's of attribute data. */
-    this.arrays = [];
-    this._debugArrays = [];
+    this.arrayBuffers = [];
 
-    /** Names of textures this object uses. */
-    this.textures = [];
-
-    this._children = [];
-
+    this.children = [];
 
     /** The animation for this object. */
     this.animation = new $W.anim.ProceduralAnimation();
 
     this._drawFunction = null;
 
+    this._drawArrays = function() {
+        return (function OBJ_drawArrays(obj, mat) {
+            try {
+                $W.GL.drawArrays(obj.type, 0, obj.vertexCount);
+            }catch (e) {
+                console.error("drawArrays Failure");
+                console.error(e);
+            }
+        });
+    };
+
+    this._drawElements = function() {
+        return (function OBJ_drawElements(obj, mat) {
+            $W.GL.bindBuffer($W.GL.ELEMENT_ARRAY_BUFFER, obj._elementBuffer);
+            try {
+                $W.GL.drawElements(obj.type, obj._elementCount, 
+                    $W.GL.UNSIGNED_SHORT, obj._elements);
+            }catch (e) {
+                console.error("drawElements Failure");
+                console.error(e);
+            }
+        });
+    };
 
 
+    /** Name of shader program used to render this object */
+    /** Add an object as a child to this object.
+     * @param {Object} obj The object to add as a child.
+     */
+    this.addChild = function(obj) {
+        this.children.push(obj);
+    };
 
+    /** Set the indices for the elements of this object.
+     * Draws this object with drawElements unless set with
+     * false.
+     * @param {Array|Boolean} elements The array of indices of the
+     * elements or false, which disabled drawElements rendering
+     * for this object.
+     */
+    this.setElements = function(elements) {
+        // don't use drawElements
+        if (elements === false) {
+            this._elements = false;
+            this._drawFunction = this._drawArrays();
+            $W.GL.bindBuffer($W.GL.ELEMENT_ARRAY_BUFFER, null);
 
-    // XXX these are also clunky
+        // use drawElements
+        }else {
+            this._elements = elements.flatten();
+            this._elementCount = this._elements.length;
+            this._elementBuffer = $W.GL.createBuffer();
+            this._drawFunction = this._drawElements();
+            $W.GL.bindBuffer($W.GL.ELEMENT_ARRAY_BUFFER, this._elementBuffer);
+            $W.GL.bufferData($W.GL.ELEMENT_ARRAY_BUFFER,new WebGLUnsignedShortArray(this._elements), 
+                    $W.GL.STATIC_DRAW);
+        }
+    };
+
+    this.setMaterial = function OBJ_setMaterial(material) {
+        if (typeof(material) === 'string') { 
+            this.material = $W.materials[material];
+        }else {
+            this.material = material;
+        }
+    };
+
+    /** Fills the array of the given name, where name is a 
+     * vertex attribute in the shader. 
+     * Also creates a buffer to hold the data in WebGL.
+     * @param {String} name The attribute variable name in a shader
+     * attached to the shader program used by this object. (this is
+     * not verified for you)
+     * @param {Array} contents The data to pass to the attribute.
+     */
+    this.fillArray = function OBJ_fillArray(name, data) {
+        data = data.flatten();
+        if (typeof(this.arrayBuffers[name]) === 'undefined') {
+            this.arrayBuffers[name] = new $W.ArrayBuffer(name, data);
+        }else {
+            this.arrayBuffers[name].setData(data);
+        }
+
+        this.arrayBuffers[name].buffer();
+    };
+
+    // These allow us to do array or element drawing without
+    // testing a boolean every frame
+
+    // drawArrays by default
+    this._drawFunction = this._drawArrays();
+    
+    /** draw this object at the given postion, rotation, and scale
+     * @param {3 Element Array} pos Position array.
+     * @param {Matrix} rot Rotation matrix.
+     * @param {3 Element Array} scale Scaling array.
+     */
+    this.drawAt = function OBJ_drawAt(pos, rot, scale) {
+            $W.modelview.push();
+
+            $W.modelview.translate(pos);
+            $W.modelview.multiply(rot);
+            $W.modelview.scale(scale);
+
+            for (var i = 0; i < this.children.length; i++) {
+                this.children[i].draw();
+            }
+
+            $W.renderer.renderObject(this, this.material, this._drawFunction);
+
+            $W.modelview.pop();
+            $W.GL.bindTexture($W.GL.TEXTURE_2D, null);
+    };
+
+    this.drawChildrenAt = function(pos, rot, scale) {
+            $W.modelview.pushMatrix();
+
+            $W.modelview.translate(pos);
+            $W.modelview.multMatrix(rot);
+            $W.modelview.scale(scale);
+
+            for (var i = 0; i < this.children.length; i++) {
+                this.children[i].draw();
+            }
+
+            $W.modelview.popMatrix();
+    };
+
+    /** draw this object at its internally stored position, rotation, and
+     * scale, INCLUDING its current animation state.
+     */
+    this.draw = function OBJ_draw() {
+        this.drawAt(
+            this.animatedPosition().elements, 
+            this.animatedRotation().matrix(),
+            this.animatedScale().elements
+        );
+    };
+
+    this.drawChildren = function() {
+        this.drawChildrenAt(
+            this.animatedPosition().elements, 
+            this.animatedRotation().matrix(),
+            this.animatedScale().elements
+        );
+    };
+
+    /** Update this object's animation state. 
+     * @param {Number} dt The delta time since the previous call to
+     * update.
+     */
+    this.update = function(dt) {
+        this.animation.update(dt);
+
+        for (var i = 0; i < this.children.length; i++) {
+            this.children[i].update(dt);
+        }
+    };
+
+    
     /** @returns {Vector} The sum of the object's base position and its 
      * animation.
      */
@@ -123,252 +305,30 @@ $W.Object = function (type, flags) {
         this.scale = $V([s,s,s]); 
     };
 
-
-
-
-
-    /** Set the shader program to the named program 
-     * @param {String} program Program name.
-     */
-    this.setShaderProgram = function (program) {
-        lastShaderProgram = this.shaderProgram;
-        this.shaderProgram = program;
-    };
-
-    this.revertShaderProgram = function(){
-        var tmp = this.shaderProgram;
-        this.shaderProgram = lastShaderProgram;
-        lastShaderProgram = tmp;
-    };
-
-    /** Add an object as a child to this object.
-     * @param {Object} obj The object to add as a child.
-     */
-    this.addChild = function(obj) {
-        this._children.push(obj);
-    };
-
-    /** Set the indices for the elements of this object.
-     * Draws this object with drawElements unless set with
-     * false.
-     * @param {Array|Boolean} elements The array of indices of the
-     * elements or false, which disabled drawElements rendering
-     * for this object.
-     */
-    this.setElements = function(elements) {
-        // don't use drawElements
-        if (elements === false) {
-            this._elements = false;
-            this._drawFunction = this._drawArrays();
-            $W.GL.bindBuffer($W.GL.ELEMENT_ARRAY_BUFFER, null);
-
-        // use drawElements
-        }else {
-            this._elements = elements.flatten();
-            this._elementCount = this._elements.length;
-            this._elementBuffer = $W.GL.createBuffer();
-            this._drawFunction = this._drawElements();
-            $W.GL.bindBuffer($W.GL.ELEMENT_ARRAY_BUFFER, this._elementBuffer);
-            $W.GL.bufferData($W.GL.ELEMENT_ARRAY_BUFFER,new WebGLUnsignedShortArray(this._elements), 
-                    $W.GL.STATIC_DRAW);
-        }
-    };
-
-    /** Fills the array of the given name, where name is a 
-     * vertex attribute in the shader. 
-     * Also creates a buffer to hold the data in WebGL.
-     * @param {String} name The attribute variable name in a shader
-     * attached to the shader program used by this object. (this is
-     * not verified for you)
-     * @param {Array} contents The data to pass to the attribute.
-     */
-    this.fillArray = function OBJ_fillArray(name, contents) {
-        console.group("Filling `" + name + "` array");
-        this._debugArrays = contents;
-
-        this.arrays[name] = new WebGLFloatArray(contents);
-        console.log(name + " array has " + contents.length + " values");
-
-        if (this.buffers[name] === undefined) {
-            this.buffers[name] = $W.GL.createBuffer();
-        }
-        this.bufferArray(name);
-        console.groupEnd();
-    };
-
-    this.bufferArray = function OBJ_bufferArray(name) {
-        console.log("Buffering `" + name + "` array");
-        var gl = $W.GL;
-        var prg= $W.programs[this.shaderProgram];
-        var atrb = prg.attributes.findByAttributeValue('name', name);
-        if (atrb === null) {
-            return;
-        }
-
-        try {
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers[atrb.name]);
-            gl.bufferData(gl.ARRAY_BUFFER, this.arrays[atrb.name], 
-                    gl.STATIC_DRAW);
-
-            gl.enableVertexAttribArray(atrb.location);
-            gl.bindBuffer(gl.ARRAY_BUFFER, null);
-        }catch (err) {
-            console.error("Object attribute buffer error");
-            console.error(atrb.name);
-            console.error(err);
-        }
-        
-    };
-
-    this.bind = function OBJ_bind() {
-        var gl = $W.GL;
-        var program = $W.programs[this.shaderProgram];
-
-        try{
-            for (var i = 0; i < program.attributes.length; i++) {
-                var atrb = program.attributes[i];
-
-                gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers[atrb.name]);
-                gl.vertexAttribPointer(atrb.location, atrb.length, 
-                        gl.FLOAT, false, 0, 0);
-                gl.bindBuffer(gl.ARRAY_BUFFER, null);
-            }
-        }catch (e) {
-            console.error("Object attribute bind error");
-            console.error(e);
-        }
-
-    };
-
-    this.setTexture = function OBJ_setTexture(texture, sampler) {
-        this.textures[0] = texture;
-        console.log("Applying `" + texture + "` texture");
-        $W.programs[this.shaderProgram].setUniformAction(sampler, 
-                function OBJ_setTexture_uniformAction(obj) {
-                    try {
-                        var gl = $W.GL;
-                        gl.activeTexture(gl.TEXTURE0);
-                        gl.bindTexture(gl.TEXTURE_2D, 
-                            $W.textures[obj.textures[0]].glTexture);
-                        gl.uniform1i(this.location, 0);
-                    }catch (e) {
-                        console.error("Texture binding error");
-                        console.error(e);
-                    }
-                });
-    };
-
-
-    // These allow us to do array or element drawing without
-    // testing a boolean every frame
-    this._drawArrays = function() {
-        return (function OBJ_drawArrays() {
-            try {
-                $W.GL.drawArrays(this.type, 0, this.vertexCount);
-            }catch (e) {
-                console.error("drawArrays Failure");
-                console.error(e);
-            }
-        });
-    };
-
-    this._drawElements = function() {
-        return (function OBJ_drawElements() {
-            $W.GL.bindBuffer($W.GL.ELEMENT_ARRAY_BUFFER, this._elementBuffer);
-            try {
-                $W.GL.drawElements(this.type, this._elementCount, 
-                    $W.GL.UNSIGNED_SHORT, this._elements);
-            }catch (e) {
-                console.error("drawElements Failure");
-                console.error(e);
-            }
-        });
-    };
-
-    // drawArrays by default
-    this._drawFunction = this._drawArrays();
-    
-    /** draw this object at the given postion, rotation, and scale
-     * @param {3 Element Array} pos Position array.
-     * @param {Matrix} rot Rotation matrix.
-     * @param {3 Element Array} scale Scaling array.
-     */
-    this.drawAt = function OBJ_drawAt(pos, rot, scale) {
-            $W.modelview.push();
-
-            $W.modelview.translate(pos);
-            $W.modelview.multiply(rot);
-            $W.modelview.scale(scale);
-
-            for (var i = 0; i < this._children.length; i++) {
-                this._children[i].draw();
-            }
-
-            try {
-                $W.programs[this.shaderProgram].use();
-                $W.programs[this.shaderProgram].processUniforms(this);
-            }catch (e) {
-                console.error("drawAt error, use/processUniforms");
-                console.error(e);
-            }
-
-            $W.modelview.pop();
-
-            this.bind();
-
-            try {
-                this._drawFunction();
-            }catch (e) {
-                console.error("draw error");
-                console.error(e);
-            }
-            $W.GL.bindTexture($W.GL.TEXTURE_2D, null);
-    };
-
-    this.drawChildrenAt = function(pos, rot, scale) {
-            $W.modelview.pushMatrix();
-
-            $W.modelview.translate(pos);
-            $W.modelview.multMatrix(rot);
-            $W.modelview.scale(scale);
-
-            for (var i = 0; i < this._children.length; i++) {
-                this._children[i].draw();
-            }
-
-            $W.modelview.popMatrix();
-    };
-
-    /** draw this object at its internally stored position, rotation, and
-     * scale, INCLUDING its current animation state.
-     */
-    this.draw = function OBJ_draw() {
-        this.drawAt(
-            this.animatedPosition().elements, 
-            this.animatedRotation().matrix(),
-            this.animatedScale().elements
-        );
-    };
-
-    this.drawChildren = function() {
-        this.drawChildrenAt(
-            this.animatedPosition().elements, 
-            this.animatedRotation().matrix(),
-            this.animatedScale().elements
-        );
-    };
-
-    /** Update this object's animation state. 
-     * @param {Number} dt The delta time since the previous call to
-     * update.
-     */
-    this.update = function(dt) {
-        this.animation.update(dt);
-
-        for (var i = 0; i < this._children.length; i++) {
-            this._children[i].update(dt);
-        }
-    };
-
     //console.groupEnd();
 };
+/** @author Benjamin DeLillo */
+/*
+     *  Copyright (c) 2009 Benjamin P. DeLillo
+     *  
+     *  Permission is hereby granted, free of charge, to any person
+     *  obtaining a copy of this software and associated documentation
+     *  files (the "Software"), to deal in the Software without
+     *  restriction, including without limitation the rights to use,
+     *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+     *  copies of the Software, and to permit persons to whom the
+     *  Software is furnished to do so, subject to the following
+     *  conditions:
+     *  
+     *  The above copyright notice and this permission notice shall be
+     *  included in all copies or substantial portions of the Software.
+     *  
+     *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+     *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+     *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+     *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+     *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+     *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+     *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+     *  OTHER DEALINGS IN THE SOFTWARE.
+*/
