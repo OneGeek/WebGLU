02535e96accf0ff052809fd538736b9cc8932ad5
Benjamin DeLillo <bpd9116@gmail.com>
2010-05-05 14:06:43 -0400

    Fixed Texture.unbind() not unbinding the texture. Added Texture.update.

--- src/Texture.js
+++ src/Texture.js
@@ -8,8 +8,10 @@ $W.Texture = function(name) {
     };
         
     this.unbind = function() {
-        $W.GL.bindTexture($W.GL.TEXTURE_2D, this.glTexture);
+        $W.GL.bindTexture($W.GL.TEXTURE_2D, null);
     };
+
+    $W.texture.update = function(){};
 
 };
 
@@ -21,11 +23,11 @@ $W.CanvasTexture = function(name, src) {
 
     this.update = function() {
         var gl = $W.GL;
-        this.texture.bind();
-        gl.texImage2D(gl.TEXTURE_2D, 0, this);
+        this.bind();
+        gl.texImage2D(gl.TEXTURE_2D, 0, this.canvas);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        this.texture.unbind();
+        this.unbind();
     };
 
 };
@@ -53,6 +55,7 @@ $W.VideoTexture = function(name, src) {
 
         this.video.texture = this;
         this.video.autobuffer = true;
+        this.video.loop = true;
         this.video.play();
         this.video.addEventListener("timeupdate", this.update, true);
     };
@@ -100,3 +103,28 @@ $W.ImageTexture = function(name, src) {
         this.setSource(src);
     }
 };
+/** @author Benjamin DeLillo */
+/*
+     *  Copyright (c) 2009 Benjamin P. DeLillo
+     *  
+     *  Permission is hereby granted, free of charge, to any person
+     *  obtaining a copy of this software and associated documentation
+     *  files (the "Software"), to deal in the Software without
+     *  restriction, including without limitation the rights to use,
+     *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+     *  copies of the Software, and to permit persons to whom the
+     *  Software is furnished to do so, subject to the following
+     *  conditions:
+     *  
+     *  The above copyright notice and this permission notice shall be
+     *  included in all copies or substantial portions of the Software.
+     *  
+     *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+     *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+     *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+     *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+     *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+     *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+     *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+     *  OTHER DEALINGS IN THE SOFTWARE.
+*/
